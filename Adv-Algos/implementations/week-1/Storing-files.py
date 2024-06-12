61e8663f1ed3c478eae91b2a989d40a0051cbb63
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-12 08:44:25 +0000

    Storing-files algos implementation

--- Adv-Algos/implementations/week-1/Storing-files.py
+++ Adv-Algos/implementations/week-1/Storing-files.py
@@ -1,1 +1,67 @@
+class FileStorage:
+    def __init__(self,N=None,file_lengths=[],frequencies=[]):
+        self.N=N
+        self.file_lengths = file_lengths
+        self.frequencies = frequencies
+    
+    def get_input(self):
+        self.N = 10 # int(input())
+        self.file_lengths = [45, 25, 20, 50, 90, 10, 20, 45, 55, 70] # list(map(int,input().split()))
+        freq = "t" # input("Entering Frequencies? T/F: ")
+        if freq.lower()=="t":
+            self.frequencies = [3,1,3,4,5,7,9,3,6,5] # list(map(int,input().split()))
+    
+    def CalculateCost(self,l):
+        N = len(l)
+        prev = l[0]
+        cost = prev
+        for i in range(1,N):
+            prev = prev + l[i]
+            cost += (prev)
+        cost = cost/N
+        return cost
 
+    def ExpectedCost(self):
+        # Mean of costs required to access the files
+        if not self.frequencies:
+            cost = self.CalculateCost(self.file_lengths)
+            self.ExpectedCost = cost
+            return cost
+        else:
+            cost = self.TotalCost(self.file_lengths,self.frequencies)
+            self.ExpectedCost = cost
+            return cost
+
+    def OptimalExpectedCost(self):
+        # If frequencies are not provided, then
+        # the files are to be ordered based on file lengths
+        if self.frequencies:
+            files = sorted([(l,f,l/f) for f,l in zip(self.frequencies,self.file_lengths)],key=lambda x:(x[2]))
+            print("Optimal Order :",files)
+            lengths_ordered = [v[0] for v in files]
+            frequencies_ordered = [v[1] for v in files]
+            cost = self.TotalCost(lengths_ordered,frequencies_ordered)/self.N
+            self.OptimalExpectedCost = cost
+            return cost
+    
+        else:
+            self.Optimal_Ordering = sorted(self.file_lengths)
+            cost = self.CalculateCost(self.Optimal_Ordering)
+            self.OptimalExpectedCost = cost
+            return cost
+    
+    def TotalCost(self,lengths_ordered,frequencies_ordered=[]):
+        if frequencies_ordered:
+            # print(lengths_ordered,frequencies_ordered)
+            prev = lengths_ordered[0]
+            cost = frequencies_ordered[0]*prev
+            for i in range(1,len(lengths_ordered)):
+                prev = prev+lengths_ordered[i]
+                cost+=frequencies_ordered[i]*(prev)
+            return cost
+        else:
+            return self.CalculateCost(lengths_ordered)*len(lengths_ordered)
+
+fileStorage = FileStorage()
+fileStorage.get_input()
+print(fileStorage.OptimalExpectedCost())
