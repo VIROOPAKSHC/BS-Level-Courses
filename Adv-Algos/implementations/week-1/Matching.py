1def3e24672e8c26b493075a84e65abe311a5760
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-12 09:44:54 +0000

    Matching-started

---
+++ Adv-Algos/implementations/week-1/Matching.py
@@ -0,0 +1,6 @@
+class StableMatching:
+    def __init__(self,preferences_men=[],preferences_women=[]):
+        self.preferences_men = preferences_men
+        self.preferences_women = preferences_women
+    
+    
