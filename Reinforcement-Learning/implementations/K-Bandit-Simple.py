81ee2158ce17a65d89211ba23e8a2f04fbff179c
Chekuri Viroopaksh <69083163+VIROOPAKSHC@users.noreply.github.com>
2024-06-22 18:13:09 +0530

    Create K-Bandit-Simple.py

---
+++ Reinforcement-Learning/implementations/K-Bandit-Simple.py
@@ -0,0 +1,45 @@
+# Author - Chekuri Viroopaksh
+# Reinforcement Learning
+# K - Arm bandit problems
+import numpy as np
+import matplotlib.pyplt as plt
+
+time_step = 1000
+epsilon = 0.1
+k = 10
+# Empty placeholders for storing total rewards and optimal actions over different timesteps in different problems
+total_rewards = np.zeros(time_step) 
+optimal_actions = np.zeros(time_step) 
+num_prob = 2000
+
+for _ in range(num_prob):
+    # Generating num_prob different k-armed bandit problems.
+    q_star = np.random.normal(0,1,10)
+    Q = np.zeros(k)
+    N = np.zeros(k)
+    rewards = np.zeros(time_step)
+    actions = np.zeros(time_step)
+    for t in range(time_step):
+        # exploration and exploitation
+        if np.random.random() < epsilon:
+            A = np.random.randint(k)
+        else:
+            A = np.argmax(Q)   
+        actions[t] = (A==np.argmax(q_star))
+
+        R = np.random.normal(q_star[A],1)
+        N[A] = N[A] + 1
+        Q[A] = Q[A] + (1/N[A]) * (R - Q[A])
+        rewards[t] = R
+        
+    total_rewards += rewards
+    optimal_actions += actions
+
+average_rewards = total_rewards/2000
+average_actions = (optimal_actions/20)
+
+plt.title("Average Rewards vs time steps")
+plt.xlabel("time step")
+plt.ylabel("Average Reward")
+plt.plot(range(time_step),average_actions)
+plt.show()
